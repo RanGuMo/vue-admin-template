319eff12d754558bb9b3079539ad219fa3206b19
MoRanGu <1126146235@qq.com>
2022-11-25 23:16:12 +0800

    平台属性管理动态展示属性 平台属性之添加属性与修改属性静态完成 收集属性名的操作 收集属性值操作 解决返回按钮数据回显问题

--- src/api/product/attr.js
+++ src/api/product/attr.js
@@ -12,3 +12,24 @@ export const reqCategory2List = (category1Id)=>request({url:`/admin/product/getC
 //获取三级分类数据接口
 ///admin/product/getCategory3/{category2Id}  get
 export const reqCategory3List = (category2Id)=>request({url:`/admin/product/getCategory3/${category2Id}`,method:'get'});
+
+//获取平台属性接口
+///admin/product/attrInfoList/{category1Id}/{category2Id}/{category3Id}  get
+export const reqAttrList = (category1Id,category2Id,category3Id)=>request({url:`/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`,method:'get'});
+
+//添加属性与属性值接口
+///admin/product/saveAttrInfo  post
+export const reqAddOrUpdateAttr = (data)=>request({url:'/admin/product/saveAttrInfo',method:'post',data});
+/*
+{
+  "attrName": "",      属性名
+  "attrValueList": [   属性名中属性值，因为属性值可以是多个，因此需要的是数组
+    {
+      "attrId": 0,          属性的id
+      "valueName": "string"  属性值
+    }
+  ],
+  "categoryId": 0,    category3Id
+  "categoryLevel":3,
+}
+*/
--- src/views/product/Attr/index.vue
+++ src/views/product/Attr/index.vue
@@ -4,7 +4,112 @@
     <el-card style="margin: 20px 0px">
       <CategorySelect @getCategoryId="getCategoryId" />
     </el-card>
-    <el-card></el-card>
+    <el-card>
+      <div v-show="isShowTable">
+        <el-button
+          type="primary"
+          style="margin: 5px 0"
+          icon="el-icon-plus"
+          :disabled="!category3Id"
+          @click="addAttr"
+          >添加属性</el-button
+        >
+        <el-table :data="attrList" style="width: 100%" border>
+          <el-table-column
+            label="序号"
+            type="index"
+            width="80"
+            align="center"
+          ></el-table-column>
+          <el-table-column
+            label="属性名称"
+            prop="attrName"
+            width="150"
+          ></el-table-column>
+          <el-table-column label="属性值列表" prop="prop" width="width">
+            <template slot-scope="{ row, $index }">
+              <el-tag
+                type="success"
+                v-for="(attrValue, index) in row.attrValueList"
+                :key="attrValue.id"
+                style="margin: 0 10px"
+              >
+                {{ attrValue.valueName }}
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" prop="prop" width="150">
+            <template slot-scope="{ row, $index }">
+              <el-button
+                type="warning"
+                icon="el-icon-edit"
+                size="mini"
+              ></el-button>
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+              ></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+
+      <!-- 添加属性 | 修改属性的结构 -->
+      <div v-show="!isShowTable">
+        <el-form :inline="true" ref="form" label-width="80px" :model="attrInfo">
+          <el-form-item label="属性名">
+            <el-input
+              placeholder="请输入属性名"
+              v-model="attrInfo.attrName"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          @click="addAttrValue"
+          :disabled="!attrInfo.attrName"
+          >添加属性值</el-button
+        >
+        <el-button @click="isShowTable = true">取消</el-button>
+
+        <el-table
+          style="width: 100%; margin: 20px 0px"
+          border
+          :data="attrInfo.attrValueList"
+        >
+          <el-table-column
+            width="80"
+            align="center"
+            type="index"
+            label="序号"
+          ></el-table-column>
+          <el-table-column width="width" prop="prop" label="属性值名称">
+            <template slot-scope="{ row, $index }">
+              <el-input
+                v-model="row.valueName"
+                placeholder="请输入属性值名称"
+                size="mini"
+              ></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column width="width" prop="prop" label="操作">
+            <template slot-scope="{ row, $index }">
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+              ></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <el-button type="primary">保存</el-button>
+        <el-button @click="isShowTable = true">取消</el-button>
+      </div>
+    </el-card>
   </div>
 </template>
 <script>
@@ -15,6 +120,19 @@ export default {
       category1Id: "",
       category2Id: "",
       category3Id: "",
+      // 接收平台属性的字段
+      attrList: [],
+      // 控制table的显示与隐藏
+      isShowTable: false,
+      //收集新增属性|修改属性使用的
+      attrInfo: {
+        attrName: "", //属性名
+        attrValueList: [
+          //属性值，因为属性值可以有多个因此用数组，每一个属性值都是一个对象需要attrId，valueName
+        ],
+        categoryId: 0, //三级分类的id
+        categoryLevel: 3, //因为服务器也需要区分几级id
+      },
     };
   },
   created() {},
@@ -34,16 +152,48 @@ export default {
         // 代表三级分类的id有了
         this.category3Id = categoryId;
         // 发起请求
+        //当用户确定三级分类的数据时候，可以向服务器发请求获取平台属性进行展示
         this.getAttrList();
       }
     },
     //获取品牌属性列表
-    getAttrList() {
+    async getAttrList() {
       // console.log("发请求");
-    }
+      const { category1Id, category2Id, category3Id } = this;
+      let result = await this.$API.attr.reqAttrList(
+        category1Id,
+        category2Id,
+        category3Id
+      );
+      // console.log(result);
+      if (result.code == 200) {
+        this.attrList = result.data;
+      }
+    },
+    // 添加属性值的回调
+    addAttrValue() {
+      //向属性值的数组里面添加元素
+      this.attrInfo.attrValueList.push({
+        attrId: undefined,
+        valueName: "",
+      });
+    },
 
-
-
+    //添加属性按钮的回调
+    addAttr() {
+      //切换table显示与隐藏
+      this.isShowTable = false;
+      //清除数据
+      //收集三级分类的id
+      this.attrInfo = {
+        attrName: "", //属性名
+        attrValueList: [
+          //属性值，因为属性值可以有多个因此用数组，每一个属性值都是一个对象需要attrId，valueName
+        ],
+        categoryId: this.category3Id, //三级分类的id
+        categoryLevel: 3, //因为服务器也需要区分几级id
+      };
+    },
   },
 };
 </script>
